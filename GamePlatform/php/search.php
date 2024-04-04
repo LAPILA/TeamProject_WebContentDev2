30a07bdab05c4e6fa486987c7afe43fc4fcfb0e1
pinkypie2128 <pinkypie2128@naver.com>
2024-04-04 22:36:44 +0900

    asaa

--- GamePlatform/php/search.php
+++ GamePlatform/php/search.php
@@ -1,14 +1,6 @@
 <?php header('Content-Type: text/html; charset=UTF-8'); ?>
-<!DOCTYPE html>
 <HTML>
 	<HEAD>      
-	<title>게임 관리 시스템</title>
-    <style>
-        body { font-family: Arial, sans-serif; }
-        table { width: 100%; border-collapse: collapse; }
-        th, td { border: 1px solid #dddddd; text-align: left; padding: 8px; }
-        th { background-color: #f2f2f2; }
-    </style>
 		<script language="javascript">
 			// 전달받은 메시지 출력
 			function showMessage( message )
@@ -26,20 +18,21 @@
 			}
 		</script>
 	</HEAD>
-
 	<BODY onLoad="showMessage( '' );" >
-	<img src="img/로고 초안.png">
 		<!-- 화면구성 -->
+		<BR> 
+		<form name = "formm" method = "post">				
+			&nbsp; &nbsp; &nbsp; 
+			책 제목 : <INPUT TYPE="text" NAME="message" SIZE="60"> 
+		</form>  
+		 &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;   
+		<INPUT TYPE = "button" value = "책 제목 검색" onClick="javascript:move( './search.php' );">
+		<INPUT TYPE = "button" value = "새 책  추가" onClick="javascript:move( './insert.php' );">	
+		<INPUT TYPE = "button" value = "책 삭제" onClick="javascript:move( './delete.php' );">	
+		<BR> <BR> &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+		<BR> <BR>  
 
-		<form name = "formm" method = "post" action="./searchResult.php">
-			<input type="text" name="searchQuery" size="60">
-        	<input type="submit" value="게임 제목 검색">
-		</form> 
-		</br>
-		<button onClick="window.location='./insert.php';">새 게임 추가</button>
-   		<button onClick="window.location='./delete.php';">게임 삭제</button>
-
+		<!-- 책 목록 출력 -->
 		<?php include("./selectSQL.php"); ?>
-
 </BODY>
 </HTML>
