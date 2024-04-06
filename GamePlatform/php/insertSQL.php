c89541f47396cb3ad234a654415a8c11616c4626
LAPILA <lapila3004@gmail.com>
2024-04-07 02:59:39 +0900

    php ver_0.2.4

--- GamePlatform/php/insertSQL.php
+++ GamePlatform/php/insertSQL.php
@@ -1,12 +1,12 @@
-<?php header('Content-Type: text/html; charset=UTF-8'); ?>
-<!DOCTYPE html>
-<?php include("./SQLconstants.php"); ?>
-
 <?php
-// 폼 데이터에서 게임 정보를 받아옵니다.
+header('Content-Type: text/html; charset=UTF-8');
+include("./SQLconstants.php");
+
+// 폼 데이터에서 게임 정보 받음
 $gameName = $_POST['game_name'];
 $price = $_POST['price'];
 $developerID = $_POST['developer_id'];
+$systemRequirement = $_POST['system_requirement']; 
 $genre = $_POST['genre'];
 $releaseDate = $_POST['release_date'];
 $imageUrl = $_POST['image_url'];
@@ -18,11 +18,11 @@ if (!$conn) {
 }
 
 // 쿼리를 준비
-$query = "INSERT INTO 게임 (게임명, 가격, 개발사ID, 장르, 출시일, 이미지URL) VALUES (?, ?, ?, ?, ?, ?)";
+$query = "INSERT INTO 게임 (게임명, 가격, 개발사ID, 시스템사양, 장르, 출시일, 이미지URL) VALUES (?, ?, ?, ?, ?, ?, ?)"; // 쿼리 수정
 $stmt = mysqli_prepare($conn, $query);
 
 // 쿼리 실행
-mysqli_stmt_bind_param($stmt, "sdisss", $gameName, $price, $developerID, $genre, $releaseDate, $imageUrl);
+mysqli_stmt_bind_param($stmt, "sdissss", $gameName, $price, $developerID, $systemRequirement, $genre, $releaseDate, $imageUrl); // 데이터 바인딩 수정
 $result = mysqli_stmt_execute($stmt);
 
 if ($result) {
@@ -34,6 +34,7 @@ if ($result) {
 // 데이터베이스 연결을 닫기
 mysqli_stmt_close($stmt);
 mysqli_close($conn);
+
 // 로그 데이터 추출 및 기록
 include("./log.php");
 writeLog($message);
