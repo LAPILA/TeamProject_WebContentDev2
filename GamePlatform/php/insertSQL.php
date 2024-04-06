495ff26bb2ed0d78823492074b6b0fdb1ea7197e
LAPILA <lapila3004@gmail.com>
2024-04-07 03:04:40 +0900

    php ver_0.2.5

--- GamePlatform/php/insertSQL.php
+++ GamePlatform/php/insertSQL.php
@@ -2,45 +2,40 @@
 header('Content-Type: text/html; charset=UTF-8');
 include("./SQLconstants.php");
 
-// 폼 데이터에서 게임 정보 받음
 $gameName = $_POST['game_name'];
-$price = $_POST['price'];
-$developerID = $_POST['developer_id'];
+$price = floatval($_POST['price']); // 가격을 실수로 변환
+$developerID = intval($_POST['developer_id']); // 개발사 ID를 정수로 변환
 $systemRequirement = $_POST['system_requirement']; 
 $genre = $_POST['genre'];
 $releaseDate = $_POST['release_date'];
 $imageUrl = $_POST['image_url'];
 
-// 데이터베이스에 연결
 $conn = mysqli_connect($mySQL_host, $mySQL_id, $mySQL_password, $mySQL_database);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// 쿼리를 준비
-$query = "INSERT INTO 게임 (게임명, 가격, 개발사ID, 시스템사양, 장르, 출시일, 이미지URL) VALUES (?, ?, ?, ?, ?, ?, ?)"; // 쿼리 수정
+$query = "INSERT INTO 게임 (게임명, 가격, 개발사ID, 시스템사양, 장르, 출시일, 이미지URL) VALUES (?, ?, ?, ?, ?, ?, ?)";
 $stmt = mysqli_prepare($conn, $query);
 
-// 쿼리 실행
-mysqli_stmt_bind_param($stmt, "sdissss", $gameName, $price, $developerID, $systemRequirement, $genre, $releaseDate, $imageUrl); // 데이터 바인딩 수정
-$result = mysqli_stmt_execute($stmt);
+mysqli_stmt_bind_param($stmt, "sdissss", $gameName, $price, $developerID, $systemRequirement, $genre, $releaseDate, $imageUrl);
 
-if ($result) {
+if (mysqli_stmt_execute($stmt)) {
     $message = "게임('" . $gameName . "')이(가) 성공적으로 추가되었습니다.";
 } else {
     $message = "게임 추가에 실패했습니다. Error: " . mysqli_error($conn);
 }
 
-// 데이터베이스 연결을 닫기
 mysqli_stmt_close($stmt);
 mysqli_close($conn);
 
-// 로그 데이터 추출 및 기록
 include("./log.php");
 writeLog($message);
 
-// 메시지 전달을 위한 HTML 폼
+echo "<script>alert('$message');</script>";
+echo "<script>window.location.href='./gameList.php';</script>";
 ?>
+
 <form name="frm" method="post" action="./gameList.php">
     <input type="hidden" name="message" value="<?php echo htmlspecialchars($message); ?>">
 </form>
